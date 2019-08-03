08d1c8b3b148e45ed2546cdb305911d7f7511c79
kmario19 <kmario_19@hotmail.com>
2019-08-03 01:49:25 -0500

    Basic Auth with JWT and express middlewares

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const port = process.env.PORT || 5000
+
+const auth = require('./routes/auth')
+const users = require('./routes/users')
+
+mongoose.connect('mongodb://localhost/condorchat', { useNewUrlParser: true })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-const Users = require('./routes/Users')
 
 app.get('/', (req, res) => {
     res.send('Ready')
 })
 
-app.use('/users', Users)
+app.use(auth)
+
+app.use('/users', users)
 
 app.use(function (req, res, next) {
     res.status(404).json({ error: 'Not found' })
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const users = express.Router()
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-
-mongoose.connect('mongodb://localhost/condorchat', { useNewUrlParser: true })
+const requireAuth = require('../middlewares/requireAuth')
 
 const User = require('../models/user')
 
-users.get('/', (req, res) => {
+users.get('/', requireAuth, (req, res) => {
     User.find({}, 'first_name last_name username', (err, users) => {
         if (err) return res.status(400).json(err)
 
@@ -15,45 +12,7 @@ users.get('/', (req, res) => {
     })
 })
 
-users.post('/login', (req, res) => {
-    User.findOne({ username: req.body.username }, '-password', (err, user) => {
-        if (err) return res.status(400).json(err)
-
-        if (user && req.body.password && bcrypt.compareSync(req.body.password, user.password))
-            return res.json(user)
-
-        return res.status(404).json({ error: 'Username or password incorrect' })
-
-    })
-})
-
-users.post('/register', (req, res) => {
-    let userData = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        username: req.body.username,
-        password: req.body.password
-    }
-
-    User.findOne({ username: userData.username }, '_id', (err, obj) => {
-        if (err) return res.status(400).json(err)
-
-        if (obj) return res.json({ error: 'User already exists' })
-
-        userData.password = bcrypt.hashSync(userData.password, 10)
-
-        let user = new User(userData)
-
-        user.save((err, user) => {
-            if (err) return res.status(400).json(err)
-
-            return res.json({ message: 'User created', user })
-        })
-
-    })
-})
-
-users.get('/:username', (req, res) => {
+users.get('/:username', requireAuth, (req, res) => {
     User.findOne({ username: new RegExp(req.params.username, 'i') }, '-password', (err, user) => {
         if (err) return res.status(400).json(err)
 
